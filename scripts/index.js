73821ca0587524a754fd9a13267aecc50627bbc1
Denis Sharikadze <denissharikadze@gmail.com>
2022-12-12 23:55:08 +0300

    task1 and task2 solved

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,85 @@
 const editButton = document.querySelector('.profile__edit-button');
-// const addButton = document.querySelector('.profile__add-button');
+const addButton = document.querySelector('.profile__add-button');
 
 const nameField = document.querySelector('.profile__name');
 const descriptionField = document.querySelector('.profile__description');
 
 const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupCardAddition = document.querySelector('.popup_type_card');
 const formElement = popup.querySelector('.popup__form');
 const nameInput = formElement.querySelector('.popup__item[name="name"]');
 const jobInput = formElement.querySelector('.popup__item[name="description"]');
-const closeButton = popup.querySelector('.popup__close-btn');
-// const saveButton = formElement.querySelector('.popup__save-btn');
+const closeButtons = document.querySelectorAll('.popup__close-btn');
+const cardTemplate = document.querySelector('#card');
+const cardsContainer = document.querySelector('.elements');
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-// popup.addEventListener('click', closePopup);
-// formElement.addEventListener('click', function(evt) {
-//   evt.stopPropagation();
-// })
-
-formElement.addEventListener('submit', formSubmitHandler); 
-
-function openPopup() {
-  popup.classList.add('popup_opened');
-  popupAutoFill();
+//Создание карточки по шаблону template с заданными значениями
+function createCard (link, name) {
+  const card = cardTemplate.content.querySelector('.element').cloneNode(true);
+  card.querySelector('.element__image').src = link;
+  card.querySelector('.element__title').textContent = name;
+  return card;
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+//Рендер карточки на страницу
+function renderCard (card) {
+  cardsContainer.prepend(card)
 }
 
+//Рендер карточек "из коробки"
+function renderInitialCards (cardsDataArray) {
+  cardsDataArray.forEach(element => {
+    renderCard(createCard(element.link, element.name));
+  });
+}
+
+//Автозаполнение input'ов значениями со станицы
 function popupAutoFill() {
   nameInput.value = nameField.textContent;
   jobInput.value = descriptionField.textContent;
 }
 
+//Открыть попап
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+  popupAutoFill();
+}
+
+//Закрыть попап
+function closePopup(evt) {
+  const target = evt.target;
+  target.parentElement.parentElement.classList.remove('popup_opened');
+}
+
+//Обработчик отправки формы "Профиль"
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     
@@ -43,3 +88,21 @@ function formSubmitHandler (evt) {
 
     closePopup();
 }
+
+//Добавление слушателя событий на все кнопки закрытия
+function addCloseButtonsListener (buttonsCollection) {
+  buttonsCollection.forEach(button => {
+    button.addEventListener('click', closePopup);
+  });
+}
+
+renderInitialCards(initialCards);
+
+editButton.addEventListener('click',() => openPopup(popupProfile));
+addButton.addEventListener('click', () => openPopup(popupCardAddition));
+// closeButton.addEventListener('click', closePopup);
+addCloseButtonsListener(closeButtons);
+
+formElement.addEventListener('submit', formSubmitHandler); 
+
+
